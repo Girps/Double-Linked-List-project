8c44867fbe9f4f20b45df53f207caabfb47c793b
Girps <jes20221168@gmail.com>
2022-05-19 15:12:33 -0700

    added functions

--- DLL.h
+++ DLL.h
@@ -22,6 +22,7 @@ class DLL
 	// Private functions memebers
 	Node* create_Node(T data_Pram) { return new Node(data_Pram);  }
 
+	// Public function memebers and constructors 
 	public: 
 		DLL() :head{ nullptr }, tail{ nullptr }, size{0}{}
 		
@@ -47,11 +48,85 @@ class DLL
 			rr_List.size = 0; 
 		}
 
-		~DLL() { std::cout << "list freed" }
+		/* Destructor deletes nodes from list */
+		~DLL() { free_Nodes(); std::cout << "list freed"; }
 
-		// Function memebers
-		DLL& operator = (const DLL& l_List) {}
-		DLL& operator = (const DLL&& rr_List) {}
+		/* Assingment overloaded operator function copies data from argument 
+			and returns a self refecence to support assignent chainging*/
+		DLL& operator = (const DLL& l_List) 
+		{
+			if (this->size > l_List.size) 
+			{
+				int diff = this->size - l_List.size; 
+				// This case remove end of tail then copy data
+				this->remove_Last(); 
+				Node* cursor = head; 
+				Node* target = l_List.head; 
+				while (cursor != nullptr) 
+				{
+					// Assign calling obj data to arg obj data
+					cursor->data = target->data; 
+					// traverse the list
+					cursor = cursor->next; 
+					target = target->next; 
+				}
+			}
+			else if (this->size < l_List.size) 
+			{
+				int diff = l_List.size - this->size;
+				Node* cursor = head; 
+				Node* target =  l_List.head;
+				// Copy data from arg list to calling object list
+				for (int i = 0; i < this->size;i++) 
+				{
+					// Copy data
+					cursor->data = l_List->data; 
+					// traverse 
+					cursor = cursor->next;
+					target = target->next; 
+				}
+				// Add remaing nodes
+				for (int i = 0; i < diff;i++) 
+				{
+					this->add_Last(target->data);
+					target = target->next; 
+				}
+			}
+			else 
+			{
+				// Lists have the same size 
+				Node* cursor = head;
+				Node* target = l_List; 
+				
+				while (cursor != nullptr) 
+				{
+					// Assign calling object node data to arg node
+					cursor->data = target->data; 
+					// Traverse the list
+					cursor = cursor->next;
+					target = target->next; 
+				}
+			}
+			// Reassign calling object size 
+			this->size = l_List.size; 
+			return *this; // return self reference
+		}
+
+		/* Move assignment overloaded function takes resources of rr list and
+			reassigns argument memebrs to null, returns self reference to support
+				assignment chaining*/
+		DLL& operator = (const DLL&& rr_List)
+		{
+			size = rr_List.size; 
+			head = rr_List.head; 
+			tail = rr_List.tail; 
+			
+			rr_List.head = nullptr;
+			rr_List.tail = nullptr; 
+			rr_List.size = 0;
+
+			return *this; 
+		}
 		
 		// getter member functions
 		void get_Size() const { return this->size;  }
@@ -79,6 +154,32 @@ class DLL
 			temp->next = cursor->next; 
 			cursor->next->prev = temp; 
 			cursor->next = temp; 
+			this->size++; // inc size
+		}
+
+		/* Void function removes a node at an index*/
+		void remove_Node(T data_Pram,int index)
+		{
+			// Throw exception if index is less than 0, equal to or greater than size
+			if (index < 0 || index >= size)
+			{
+				throw out_of_range;
+			}
+			int offset{ index - 1 };
+			// Cursor hold address in head pointer
+			Node* target = head;
+			// Find the offset 
+			while (index > 0)
+			{
+				target = target->next;
+				offset--;
+			}
+			Node* cursor = target->prev; 
+			// Now reassing pointers and delete the targeted node
+			cursor->next = target->next; 
+			target->next->prev = cursor; 
+			delete target; // remove node from the list
+			this->size--; // inc size
 		}
 
 		/*Void function allocates node and assigns address at beginning of the list*/
@@ -131,13 +232,14 @@ class DLL
 				// assign pointer to head
 				Node* cursor = head; 
 				head = head->next; // next node is head
-				head->prev = nullptr
+				head->prev = nullptr;
 				// delete old node
 				delete cursor;
 				// dec size
 				this->size--; 
 			}	
 		}
+		
 		/* Void function removes last node in linked list and decrements size*/
 		void remove_Last() 
 		{
@@ -163,11 +265,99 @@ class DLL
 			while (cursor != nullptr) 
 			{
 				// Delete node prior to next node
-				cursor->next = cursor;
+				cursor = cursor->next;
 				delete temp; 
-				temp->next = temp; 
+				temp = cursor; 
 			}
 		}
 
+		/* Bool returning function linear seaches list for data passsed in the arguement*/
+		bool find_Node(T data_Pram) 
+		{
+			Node* cursor = head; 
+			// Traverse list until the end or return when data is found
+			while (cursor != nullptr) 
+			{
+				// Assign pointer to next node
+				cursor = cursor->next; 
+				if (cursor->data == data_Pram)  
+				{
+					return true; 
+				}
+			}
+			// Not found return false 
+			return false; 
+		}
 
+		/* Void function iterates the list and prints node data*/
+		void traverse() 
+		{
+			// Pointer to the head
+			Node* cursor = head;
+			// Pointer is not null assign to next node
+			while (cursor != nullptr) 
+			{
+				std::cout << "[" << cursor->data << "]===";
+				cursor = cursor->next; 
+			}
+			std::cout << "NULL"; 
+		}
+
+		/*Void returning function reverse linked list and reassgins head and tail*/
+		void reverse() 
+		{
+			Node* new_Head{this->tail};
+			Node* new_Tail{this->head};
+			// head assigned tail address 
+			// tail assigned head address
+
+			reverse_Helper(head); 
+
+			head = new_Head; 
+			tail = new_Tail; 
+		}
+
+		/* Recursive function */
+		Node* reverse_Helper(Node* &ptr) 
+		{
+			if (ptr == nullptr)
+			{
+				return ptr;
+			}
+			Node* temp = (ptr)->prev;
+			(ptr)->prev = (ptr)->next;
+			(ptr)->next = temp;
+
+			if ((ptr)->prev == nullptr)
+			{
+				return nullptr;
+			}
+			else
+			{
+				return  reverse_Helper(ptr->prev);
+			}
+		}
+
+		/*Bool returning overlaoded function compares size and data between
+			linked list*/
+		bool operator == (const DLL &arg_List) 
+		{
+			// same size or false
+			if (this->size == arg_List.size) 
+			{
+				Node* cursor = head;
+				Node* target = arg_List.head; 
+				while (cursor != nullptr) 
+				{
+					// If data doesn't match return false
+					if (cursor->data != arg_List->data) 
+					{
+						return false;
+					}
+				}
+				// Return true if size and data are the same
+				return true; 
+			}
+			return false;
+		}
 }; 
--- Driver.cpp
+++ Driver.cpp
@@ -1,10 +1,67 @@
 #include <iostream>
+#include "DLL.h"
+#include <stack>
+
+struct Node
+{
+	Node* next;
+	Node* prev; 
+	int data; 
+	Node(int data_Pram) :data{ data_Pram }, next{ nullptr }, prev{nullptr} {}
+	~Node() {}
+}; 
 
 
+void traverse( Node* cursor) ; 
+Node* rec_Rev(Node** ptr);
 
 
 int main() 
 {
-	std::cout << "Hello world"; 
+	DLL<int> list;
+	list.add_First(1);
+	list.add_First(2); 
+	list.add_First(3);
+	list.traverse(); 
+	list.reverse();
+	list.traverse(); 
 
+	/*for (int i = 0; i < 3; i++)
+	{
+		myStack.push(cursor_Tra);
+		cursor_Tra = cursor_Tra->next;
+	}
+	Node* target{ nullptr };
+	for (int i = 0; i < 3; i++)
+	{
+		target = myStack.top();
+		target->prev = target->next;
+		myStack.pop();
+		if (!(myStack.empty()))
+		{
+			target->next = myStack.top();
+		}
+		else 
+		{
+			target->next = nullptr; 
+		}
+	}
+
+	head = new_Head;
+	tail = new_tail;*/
+
+
+	return 0; 
+}
+
+
+void traverse( Node* pram_Cur)  
+{
+	Node* cursor = pram_Cur; 
+	while (cursor != nullptr) 
+	{
+		printf("[%d]---",cursor->data);
+		cursor = cursor->next; 
+	}
+	printf("NULL");
 }
